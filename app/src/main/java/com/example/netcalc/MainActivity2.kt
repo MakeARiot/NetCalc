56f6c0843a50dc5c5bfa9ce5535e0a02c78857e0
Kirill <kkramarskiy@gmail.com>
2022-04-26 22:00:41 +0400

    добавил wildcard

--- app/src/main/java/com/example/netcalc/MainActivity2.kt
+++ app/src/main/java/com/example/netcalc/MainActivity2.kt
@@ -18,35 +18,46 @@ class MainActivity2 : AppCompatActivity() {
         val addressView = findViewById<TextView>(R.id.address)
         val address16View = findViewById<TextView>(R.id.address16)
         val bitmaskView = findViewById<TextView>(R.id.Bitmask)
+        val netmaskView = findViewById<TextView>(R.id.netmask)
+        val netmask16View = findViewById<TextView>(R.id.netmask16)
+        val wildcardView = findViewById<TextView>(R.id.wildcard)
+        val wildcard16View = findViewById<TextView>(R.id.wildcard16)
 
         val ip1String = intent.extras?.get("ip1").toString()
         val ip2String = intent.extras?.get("ip2").toString()
         val maskString = intent.extras?.get("mask").toString()
 
+        var wildcard = ""
         try {
             val mask = ArrayList<UByte>()
             maskString.split(" - ")[1].split(".").forEach { b ->
                 mask.add(b.toUByte())
             }
-        } catch (e: Exception){ Log.d("MyLog", "31 $e") }
+            mask.forEach { b ->  wildcard += "${b.inv()}." }
+        } catch (e: Exception){ Log.d("MyLog", "$e") }
         try {
             val ip1 = ArrayList<UByte>()
             ip1String.split(".").forEach { b ->
                 ip1.add(b.toUByte())
             }
 
-        } catch (e: Exception){ Log.d("MyLog", "38 $e")}
+        } catch (e: Exception){ Log.d("MyLog", "$e")}
         try {
             val ip2 = ArrayList<UByte>()
             ip2String.split(".").forEach { b ->
                 ip2.add(b.toUByte())
             }
 
-        } catch (e: Exception){ Log.d("MyLog", "45 $e")}
+        } catch (e: Exception){ Log.d("MyLog", "$e") }
 
         //-----------------------
         addressView.text = "Address: $ip1String"
-//        address16View.text = "16-ричный: ${}" todo: 16-ричный
+//        address16View.text = "${}" todo: 16-ричный
         bitmaskView.text = "Bitmask: ${maskString.split(" - ")[0]}"
+        netmaskView.text = "Netmask: ${maskString.split(" - ")[1]}"
+//        netmask16View.text = "${}" todo: 16-ричный
+        wildcardView.text = "Wildcard: ${wildcard.removeSuffix(".")}"
+//        wildcard16View.text = "" todo: 16-ричный
+
     }
 }
